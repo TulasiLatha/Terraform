1980a27465a3e52292d4e323fad0d804569f9eb2
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2024-12-24 19:21:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-01816d07b1128cd2d"
 instance_type = "t2.medium"
-key_name = "tulasi"
+key_name = "rahul"
 vpc_security_group_ids = ["sg-01cf1cf01df9cb190"]
 tags = {
 Name = var.instance_names[count.index]
